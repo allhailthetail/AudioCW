52b9e20d34a26fb49ae67562b65dfb969ae7dc5a
Matthew Younger <matthewyounger33@gmail.com>
2026-09-11 10:10:32 -0500

    added low-pass filter option.

--- cw/signal.py
+++ cw/signal.py
@@ -1,10 +1,15 @@
+from cw.methods import _one_pole_lowpass
 from cw.methods import _mix_m_t, _ook_sr_convert, _save_wav, _word_to_ook_tau
 
-def generate_audio(message, wpm = 10, frequency=700, sample_rate = 8000):
+def generate_audio(message, wpm = 10, frequency=700, sample_rate = 8000, filtering = "1p_lpf"):
     raw_ook_sn = _word_to_ook_tau(message)
     m_t = _ook_sr_convert(raw_ook_sn, wpm = wpm, sample_rate = sample_rate)
-    final_signal = _mix_m_t(m_t, frequency = frequency, sample_rate = sample_rate)
-    return final_signal
+    ct_mt = _mix_m_t(m_t, frequency = frequency, sample_rate = sample_rate)
+    
+    if filtering == "1p_lpf":
+        return _one_pole_lowpass(ct_mt, carrier_frequency = frequency, sample_rate = sample_rate)
+    else:
+        return ct_mt
 
 def write_wav(signal, sample_rate = 8000, path = "output.wav"):
     _save_wav(signal = signal, sample_rate = sample_rate, path = path)
